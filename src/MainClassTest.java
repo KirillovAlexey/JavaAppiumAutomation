50ac062238e24a67b67dbc2a182c733880c7efb2
KirillovAlexey <rikxela@gmail.com>
2021-11-08 01:11:58 +0300

    Задание №1
    Изменено наименование метода в классе MainClassTest

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,9 +3,9 @@ import org.junit.Test;
 
 public class MainClassTest {
     @Test()
-    public void MainClassTest() {
-        MainClass mainClass = new MainClass();
+    public void testGetLocalNumber()
+    {
         Assert.assertEquals("Число которое вернул метод getLocalNumber() не равняется 14",
-                14, mainClass.getLocalNumber());
+                14, new MainClass().getLocalNumber());
     }
 }
